dceda1ce79548ff97d6ee42a1b1d0039d44b9c5d
Jervis <jervis.lopes20@gmail.com>
2024-01-16 20:00:10 +0530

    undid bugs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HeroImage from "../assets/heroImage.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
 
 
 const Home = () => {
@@ -21,16 +22,19 @@ science education.Strong grasp of the fundamentals
 of object-oriented programming
 and adept in Java, Python, and C++.
           </p>
-
+          <Link to='portfolio' smooth duration={500}>
           <div
               duration={500}
               className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
             >
+              
               Portfolio
+             
               <span className="group-hover:rotate-90 duration-300">
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
           </div>
+          </Link>
         </div>
 
         <div>
